dd4dd5298645dc4d4c5362d60b4c221d0ffc997e
Jonathan Gan <jonathang132298@gmail.com>
2025-02-02 09:41:02 -0500

    damn sector performance

--- app/api/sector-performance/route.ts
+++ app/api/sector-performance/route.ts
@@ -3,16 +3,10 @@ import { NextResponse } from "next/server"
 export async function GET() {
     try {
         const url = `https://financialmodelingprep.com/api/v3/sector-performance?apikey=${process.env.FMP_API_KEY}`
-        const options = {
-            method: "GET",
-            next: {
-                revalidate: 3600,
-            },
-        }
-        const res = await fetch(url, options)
+        const res = await fetch(url)
 
         if (!res.ok) {
-            throw new Error("Failed to fetch sector performance")
+           console.error("Failed to fetch sector performance: ", res)
         }
         return NextResponse.json(await res.json())
     } catch (error) {
