2bdbfc09000c0c7aa76a3d5d1f83c726c3e4089d
Jonathan Gan <jonathang132298@gmail.com>
2025-04-15 01:40:07 -0400

    fixing light mode

--- components/realestate/RealEstateMarketTrends.tsx
+++ components/realestate/RealEstateMarketTrends.tsx
@@ -153,7 +153,6 @@ export default function RealEstateMarketTrends() {
                 overflow: 'hidden',
               }} 
               loading="lazy"
-              allowTransparency={true}
             ></iframe>
           )}
         </div>
