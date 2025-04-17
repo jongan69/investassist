48fa7f728b8b577a90484edf20d8c6682298c227
Jonathan Gan <jonathang132298@gmail.com>
2025-04-17 02:24:05 -0400

    fixning up user page

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
-import WelcomeEmail from '@/components/email/WelcomeEmail';
+import { WelcomeEmail } from '@/components/email/WelcomeEmail';
 
 // Initialize Resend with your API key
 const resend = new Resend(process.env.RESEND_API_KEY);
